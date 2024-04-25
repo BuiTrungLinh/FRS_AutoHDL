ab690db7279fdbbc44147ac81a9c18c454901fb8
buitr <buitrunglinh2708@gmail.com>
2024-04-25 21:27:38 +0700

    Linh - add ignore file, dlrmus

---
+++ MetaData/common.py
@@ -0,0 +1,9 @@
+class dlrmus:
+    aaa = "aaaa"
+    bbb = "bbb"
+
+
+class ihs:
+    i = "identification"
+    h = "heath"
+    s = "statictics"
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+import os
+import subprocess
+from MetaData import common as common_data
 
 import connection as con
 
 if __name__ == '__main__':
-    SP = con.get_service_ports_list()
-    if not SP["isFoundSP"]:
-        print('Warning: Could not find any Datalogic\'s "ServicePort"')
-    else:
-        print('Interface: {}, PortName: {}'.format(SP["current_interface"], SP["current_sp_name"]))
-        sp_port = con.Connection(port=SP["current_sp_name"])
-        sp_port.open_port()
-        print(sp_port.send_command('011C'))
-        # idenStr = sp_port.send_command('011C').decode('utf-8', errors='ignore')
-        # idenList = idenStr.replace('\\x03', '').split('\\x02')
-        # for i in idenList:
-        #     if i.startswith('A'):
-        #         print(i)
-        sp_port.close_port()
+    subprocess.run("Tools\\DLRMUs\\dlrmus.exe -a ServicePort")
+    # SP = con.get_service_ports_list()
+    # if not SP["isFoundSP"]:
+    #     print('Warning: Could not find any Datalogic\'s "ServicePort"')
+    # else:
+    #     print('Interface: {}, PortName: {}'.format(SP["current_interface"], SP["current_sp_name"]))
+    #     sp_port = con.Connection(port=SP["current_sp_name"])
+    #     sp_port.open_port()
+    #     idenStr = sp_port.send_command('011C').decode('utf-8', errors='ignore')
+    #     idenList = idenStr.replace('\\x03', '').split('\\x02')
+    #     for i in idenList:
+    #         if i.startswith('A'):
+    #             print(i)
+    #     sp_port.close_port()
