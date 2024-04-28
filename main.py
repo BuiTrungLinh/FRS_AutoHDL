9961f2e9abb126d59d887d8296f01551d785198b
buitr <buitrunglinh2708@gmail.com>
2024-04-28 12:12:52 +0700

    Linh - Update method conection

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import os
-import subprocess
-from MetaData import common as common_data
-
+import serial
+import time
 import connection as con
 
 if __name__ == '__main__':
-    subprocess.run("Tools\\DLRMUs\\dlrmus.exe -a ServicePort")
-    # SP = con.get_service_ports_list()
-    # if not SP["isFoundSP"]:
-    #     print('Warning: Could not find any Datalogic\'s "ServicePort"')
-    # else:
-    #     print('Interface: {}, PortName: {}'.format(SP["current_interface"], SP["current_sp_name"]))
-    #     sp_port = con.Connection(port=SP["current_sp_name"])
-    #     sp_port.open_port()
-    #     idenStr = sp_port.send_command('011C').decode('utf-8', errors='ignore')
-    #     idenList = idenStr.replace('\\x03', '').split('\\x02')
-    #     for i in idenList:
-    #         if i.startswith('A'):
-    #             print(i)
-    #     sp_port.close_port()
+
+    sp_comport = con.get_service_ports_list()
+    if not sp_comport["isFoundSP"]:
+        print('Warning: Could not find any Datalogic\'s "ServicePort"')
+    else:
+        print('Interface: {}, PortName: {}'.format(sp_comport["current_interface"], sp_comport["current_sp_name"]))
+        sp = con.Connection(port=sp_comport["current_sp_name"])
+        sp.open_port()
+        idenStr = str(sp.send_command('011C'))
+        print(idenStr)
+        # idenList = idenStr.replace('\\x03', '').split('\\x02')
+        # for i in idenList:
+        #     if i.startswith('A'):
+        #         print(i)
+        sp.close_port()
