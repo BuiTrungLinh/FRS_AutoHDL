7a9eb9ea228e1828ad74d2d394f6a8c07ac4de84
buitr <buitrunglinh2708@gmail.com>
2024-05-12 16:39:31 +0700

    Linh - Update Gui

---
+++ MetaData/testcase_data.py
@@ -0,0 +1,8 @@
+class FormatTcs:
+    interface = 'interface'
+    file_type = 'file_type'
+    update_type = 'update_type'
+    build_from = 'build_from'
+    build_to = 'build_to'
+    prefix = 'TC_FU_HDL'
+    upos = 'UPOS'
